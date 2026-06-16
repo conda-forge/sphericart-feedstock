e6facde50b73470794a4e29696bd6cf330f2a323
jeongseok-meta <142548112+jeongseok-meta@users.noreply.github.com>
2026-06-16 07:00:27 -0700

    Fix CUDA torch build for sphericart 2.0.0

--- recipe/build_torch.sh
+++ recipe/build_torch.sh
@@ -5,6 +5,10 @@ set -eu
 export TORCH_CUDA_ARCH_LIST="5.0;6.0;7.0;7.5;8.0;8.6;9.0;10.0;12.0+PTX"
 export SPHERICART_ARCH_NATIVE="OFF"
 
+if [[ -d "${PREFIX}/include/nvtx3" ]]; then
+    export CMAKE_ARGS="${CMAKE_ARGS:-} -Dnvtx3_dir=${PREFIX}/include"
+fi
+
 cd sphericart-torch
 
 "${PYTHON}" -m pip install . -vv --no-deps --no-build-isolation
